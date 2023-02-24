2164c1fdb344dc5ad2ffd59aed20c1888e0c4dfe
Niklas Burg <niklasburg1999.nb@gmail.com>
2023-02-24 14:05:53 +0100

    changed to HashRouter

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -2,7 +2,7 @@ export async function getUser(user_id) {
   let token = "Token " + localStorage.getItem("tokenKanban");
 
   let response = await fetch(
-    "http://127.0.0.1:8000/get/user/?user_id=" + user_id,
+    "http://34.147.74.31/get/user/?user_id=" + user_id,
     {
       method: "GET",
       headers: {
--- src/store/apis/tasksApi.js
+++ src/store/apis/tasksApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 let tasksApi = createApi({
   reducerPath: "tasks",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://127.0.0.1:8000/",
+    baseUrl: "http://34.147.74.31/",
   }),
   endpoints(builder) {
     return {
